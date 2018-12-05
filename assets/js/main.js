f31d2740876dac7a0ed4cc69337300204b7a26b0
elf_gzp <741424975@qq.com>
2018-12-05 10:17:31 +0800

    [MOD] hide hacknical when use mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,3 +39,22 @@ function showRight() {
 function goneRight() {
   rightTab.setAttribute("class","rotateOutDownRight self-animated");
 }
+
+var os = function() {
+  var ua = navigator.userAgent,
+    isWindowsPhone = /(?:Windows Phone)/.test(ua),
+    isSymbian = /(?:SymbianOS)/.test(ua) || isWindowsPhone,
+    isAndroid = /(?:Android)/.test(ua),
+    isFireFox = /(?:Firefox)/.test(ua),
+    isChrome = /(?:Chrome|CriOS)/.test(ua),
+    isTablet = /(?:iPad|PlayBook)/.test(ua) || (isAndroid && !/(?:Mobile)/.test(ua)) || (isFireFox && /(?:Tablet)/.test(ua)),
+    isPhone = /(?:iPhone)/.test(ua) && !isTablet,
+    isPc = !isPhone && !isAndroid && !isSymbian;
+  return {
+    isTablet: isTablet,
+    isPhone: isPhone,
+    isAndroid : isAndroid,
+    isPc : isPc
+  };
+}();
+
