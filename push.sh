9d0932aeba93f08517e3542de73c3227fd701685
Gzp_ <741424975@qq.com>
2018-12-10 18:27:08 +0800

    [MOD] modify push.sh

--- push.sh
+++ push.sh
@@ -6,6 +6,8 @@ git checkout --orphan gh-pages
 shopt -s extglob
 rm -rf !(_site) && mv _site/* . && rm -rf _site
 touch .nojekyll
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 git add .
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 git push origin gh-pages -f
