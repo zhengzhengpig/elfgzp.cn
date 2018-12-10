d601350c74ec584ca899659a26393c9fdf7226c9
Gzp_ <741424975@qq.com>
2018-12-10 17:56:49 +0800

    [MOD] modify push.sh

--- push.sh
+++ push.sh
@@ -1,19 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-setup_git() {
-  git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis CI"
-}
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git checkout --orphan gh-pages
+shopt -s extglob
+rm -rf !(_site) && mv _site/* . && rm -rf _site
+git add .
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+git push origin gh-pages -f
 
-commit_website_files() {
-  git checkout --orphan gh-pages
-  shopt -s extglob
-  rm -rf !(_site) && mv _site/* . && rm -rf _site
-  git add .
-  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
-  git push origin gh-pages -f
-}
-
-
-setup_git
-commit_website_files
