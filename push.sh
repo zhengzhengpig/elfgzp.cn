babf3e59ba013c058986ce68d019fbc5859fcadf
Gzp_ <741424975@qq.com>
2018-12-10 18:03:35 +0800

    [MOD] modify push.sh

--- push.sh
+++ push.sh
@@ -5,6 +5,7 @@ git config --global user.name "Travis CI"
 git checkout --orphan gh-pages
 shopt -s extglob
 rm -rf !(_site) && mv _site/* . && rm -rf _site
+touch .nojekyll
 git add .
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 git push origin gh-pages -f
